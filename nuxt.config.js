093f89d039069c88f5c4b6d6ac9501a821609eca
mondaychen <monday.chen@gmail.com>
2019-05-30 18:07:45 -0400

    add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,39 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon/favicon-32x32.png',
+        sizes: '32x32'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon/favicon-194x194.png',
+        sizes: '194x194'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon/favicon-96x96.png',
+        sizes: '96x96'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon/android-chrome-192x192.png',
+        sizes: '192x192'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon/favicon-16x16.png',
+        sizes: '16x16'
+      }
+    ]
   },
 
   /*
