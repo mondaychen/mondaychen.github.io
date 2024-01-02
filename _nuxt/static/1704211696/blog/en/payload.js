26e09ead9164a98d90a1ba9fe8c1d864b6e2a86b
mondaychen <monday.chen@gmail.com>
2024-01-02 11:08:26 -0500

    fc9a7b9

---
+++ _nuxt/static/1704211696/blog/en/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/blog/en", {data:[{posts:[{title:"A Workaround for offset re-location in Single-Page Apps",time:1436500401017,link:"\u002Fblog\u002Fen\u002FA-Workaround-for-offset-relocation-in-Single-Page-Apps"}]}],fetch:{},mutations:void 0});
